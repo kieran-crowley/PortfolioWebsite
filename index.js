1045dbb4c916175c94464ae46da8fa7600e55ead
kieran crowley <kierancrowley@hotmail.co.uk>
2024-05-26 19:05:29 +0100

    added buttons

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ let elements = [
     // 'navigation__item'
 ];
 
+let courasel = document.querySelectorAll('.middle-courasel');
+console.log(courasel)
+
 document.onpointerover = (e) => {
     elements.forEach(item => {
         if(e.target.classList.contains(item)) {
-            addEventListers(e, item);
+            scale(e, item);
         }
     })
 }
 
-function addEventListers(e, element) {
+courasel.forEach(item => item.onclick = () => console.log('clicked'))
+
+
+
+
+
+
+
+function scale(e, element) {
     e.target.classList.add(element + '--selected')
 
     e.target.onpointerout = () => {
         if(e.target.classList)
         e.target.classList.remove(element + '--selected')
     }
-    
 }
